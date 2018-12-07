529b057162a18d2dd573f47565e090b0b9a73067
idlviv <idlviv@gmail.com>
2018-12-07 15:32:52 +0200

    +

--- 002/server.js
+++ 002/server.js
@@ -1,32 +1,53 @@
 const http = require('http');
-const fs = require('fs');
+// const fs = require('fs');
 
+var static = require('node-static');
+ 
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('.');
 server = http.createServer((req, res) => {
     console.log('server run');
 
 
-    const { headers, method, url } = req; // const headers = req.headers ..etc
-    console.log('req.headers', headers);
-    console.log('req.method', method);
-    console.log('req.url', url);
-    console.log('req.body', req.body); // post, put
-    
-    fs.readFile('./index.html', (err, file) => {
-        if (err) {
-            throw err; 
-        };
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-            'yo': 'bacon'
+//     const { headers, method, url } = req; // const headers = req.headers ..etc
+//     console.log('req.headers', headers);
+//     console.log('req.method', method);
+//     console.log('req.url', url);
+//     console.log('req.body', req.body); // post, put
+    req.addListener('end', function () {
+        fs.readFile('./index.html', (err, file) => {
+            if (err) {
+                throw err; 
+            };
+            file.serve(req, res);
         });
-        res.write(file);
-        res.end();
-    });
-    
+        
+    }).resume();
+   
 
+//     routs
+    
 
 });
+
+// function routs(req, res) {
+//     if (req.routs === 'getroute') {
+
+//         res.writeHead(200, {
+//             'Content-Type': 'text/json',
+//         });
+//         res.end('ok');
+        
+//     } else {
+//         res.end('not ok');
+//     }
+
+// }
 
 server.listen(8080, () => {
     console.log('on port 8080');
 })
+
+
